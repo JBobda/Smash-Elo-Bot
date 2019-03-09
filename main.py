600b1579c567c1b00d8cab46244486ce35d6ba99
JBobda <J.BobdaT@gmail.com>
2019-03-09 12:58:39 -0600

    Finished the leaderboard command that gets the players from the firebase server, sorts them by Elo and then lists them out in the discord

--- main.py
+++ main.py
@@ -22,6 +22,36 @@ async def get_elo(ctx):
     channel = ctx.message.channel
     await client.send_message(channel, str(author.name) + "'s elo is " + str(elo_score))
 
+@client.command(
+    name='board',
+    pass_context=True
+)
+async def display_leaderboard(ctx):
+    server = ctx.message.author.server
+    channel = ctx.message.channel
+    leaders = current_player = firebase.get('/members', None)
+    leader_list = list(leaders.values())
+    leader_list.sort(key=lambda k: k['elo_score'], reverse=True)
+
+    await client.send_message(channel, 
+    """
+    The top Smash Players in {}:
+    1. {} : {}
+    2. {} : {}
+    3. {} : {}
+    4. {} : {}
+    5. {} : {}
+    """
+    .format(server.name,
+        leader_list[0]['name'], leader_list[0]['elo_score'],
+        leader_list[1]['name'], leader_list[1]['elo_score'],
+        leader_list[2]['name'], leader_list[2]['elo_score'],
+        leader_list[3]['name'], leader_list[3]['elo_score'],
+        leader_list[4]['name'], leader_list[4]['elo_score'],))
+        
+
+    
+
 @client.event
 async def on_ready():
     print("Logged in as")
